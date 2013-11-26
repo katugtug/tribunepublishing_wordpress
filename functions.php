34d3a39f6c01b638072354f26eddfcb17a223471
katugtug <bricebertels@gmail.com>
2013-11-26 13:00:15 -0600

    loop back in

--- functions.php
+++ functions.php
@@ -2,6 +2,4 @@
 function register_my_menu() {
     register_nav_menu('header-menu',__( 'Header Menu' ));
     }
-    add_action( 'init', 'register_my_menu' );
-
-?> 
+add_action( 'init', 'register_my_menu' );?> 
